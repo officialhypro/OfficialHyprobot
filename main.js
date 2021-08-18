c8d4e1a1c5f3d356cf92930b6e89525d243ac31f
oofquest <46395745+oofquest@users.noreply.github.com>
2021-08-17 21:24:50 -0400

    renamed a bunch of things

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ ws: { properties: { $browser: "Discord iOS" }} }); // if you dont want the bot status to be on mobile remove { ws: { properties: { $browser: "Discord iOS" }}  ok cool
-const info = require("./configure/secret.json");
+const info = require("./configure/config.json");
 
 client.commands = new Discord.Collection();
 
-const lib = require("./lib/functions");
-lib.setup(client);
+const load = require("./load/functions");
+load.setup(client);
 
 module.exports.client = client;
 
