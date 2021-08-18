bd3e2ddce5bc4c43a205a59615a67afca578c61c
oofquest <46395745+oofquest@users.noreply.github.com>
2021-08-17 21:21:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({ ws: { properties: { $browser: "Discord iOS" }} }); // if you dont want the bot status to be on mobile remove { ws: { properties: { $browser: "Discord iOS" }}  ok cool
 const info = require("./configure/secret.json");
 
 client.commands = new Discord.Collection();
